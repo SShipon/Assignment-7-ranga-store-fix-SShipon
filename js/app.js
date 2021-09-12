6d8e992b454fa58f2f5de1cb39f2ef5e888e02b5
SShipon <shamsulislamshipon2022@gmail.com>
2021-09-13 02:12:08 +0600

    hello

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <P></P>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
@@ -78,3 +79,4 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+updateTotal 
